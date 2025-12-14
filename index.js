c5ae60498a59aec00161acca7c81c242a9997d75
walkwel-tech-ac <aayush.chauhan@walkweltech.com>
2025-12-14 10:51:09 +0530

    sdfsdf

--- index.js
+++ index.js
@@ -281,7 +281,7 @@ app.get("/health",(req,res)=>{
     res.status(200).json({success:true,message:"app health was good"})
 })
 
-app.get("/env2",((req,res)=>{
+app.get("/env3",((req,res)=>{
     res.status(200).json({success:true,env})
 }))
 
